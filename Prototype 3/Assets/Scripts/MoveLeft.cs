f76224f83e8b94512f8e00b329d486d2b6feeb4b
Chase <chase.hyska@gmail.com>
2023-05-02 13:42:21 -0400

    unit 3.2

--- Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
@@ -7,12 +8,22 @@ public class MoveLeft : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript =
+            GameObject.Find("Player").GetComponent<PlayerController>();
     }
     private float speed = 30;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -23,8 +23,17 @@ public class PlayerController : MonoBehaviour
             isOnGround = false;
         }
     }
+    public bool gameOver = false;
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 }
